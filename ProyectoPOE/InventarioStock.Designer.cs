73f220b407de7998ddb9b1fc4ad1d6238cc5a561
SALT0S <josesansalt@gmail.com>
2022-07-10 19:31:09 -0500

    Arreglando codigo

--- ProyectoPOE/InventarioStock.Designer.cs
+++ ProyectoPOE/InventarioStock.Designer.cs
@@ -30,9 +30,9 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.cmb_FiltrarStock = new System.Windows.Forms.ComboBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.listView1 = new System.Windows.Forms.ListView();
+            this.lv_ListaProductos = new System.Windows.Forms.ListView();
             this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
             this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
             this.columnHeader3 = new System.Windows.Forms.ColumnHeader();
@@ -43,7 +43,7 @@
             this.columnHeader8 = new System.Windows.Forms.ColumnHeader();
             this.columnHeader9 = new System.Windows.Forms.ColumnHeader();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.lbl_CantidadRegistros = new System.Windows.Forms.Label();
             this.btn_Regresar = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
@@ -51,7 +51,7 @@
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.DarkGray;
-            this.button1.Font = new System.Drawing.Font("Segoe Print", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.button1.Location = new System.Drawing.Point(820, 65);
             this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
@@ -63,7 +63,7 @@
             // button2
             // 
             this.button2.BackColor = System.Drawing.Color.DarkGray;
-            this.button2.Font = new System.Drawing.Font("Segoe Print", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.button2.Location = new System.Drawing.Point(1067, 65);
             this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button2.Name = "button2";
@@ -72,22 +72,35 @@
             this.button2.Text = "- Eliminar Producto";
             this.button2.UseVisualStyleBackColor = false;
             // 
-            // comboBox1
+            // cmb_FiltrarStock
             // 
-            this.comboBox1.BackColor = System.Drawing.Color.DarkGray;
-            this.comboBox1.Font = new System.Drawing.Font("Segoe Print", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(21, 64);
-            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(234, 48);
-            this.comboBox1.TabIndex = 2;
-            this.comboBox1.Text = "Filtrar";
+            this.cmb_FiltrarStock.BackColor = System.Drawing.Color.DarkGray;
+            this.cmb_FiltrarStock.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmb_FiltrarStock.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.cmb_FiltrarStock.FormattingEnabled = true;
+            this.cmb_FiltrarStock.Items.AddRange(new object[] {
+            "ABARROTES",
+            "PRODUCTOS ENLATADOS",
+            "LÁCTEOS",
+            "BOTANAS",
+            "CONFITERÍA/DULCERIA",
+            "HARINAS Y PAN",
+            "FRUTAS Y VERDURAS",
+            "BEBIDAS",
+            "ALIMENTOS PREPARADOS",
+            "AUTOMEDICACIÓN",
+            "HIGIENE PERSONAL:"});
+            this.cmb_FiltrarStock.Location = new System.Drawing.Point(21, 85);
+            this.cmb_FiltrarStock.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cmb_FiltrarStock.Name = "cmb_FiltrarStock";
+            this.cmb_FiltrarStock.Size = new System.Drawing.Size(234, 37);
+            this.cmb_FiltrarStock.TabIndex = 2;
+            this.cmb_FiltrarStock.SelectedIndexChanged += new System.EventHandler(this.cmb_FiltrarStock_SelectedIndexChanged);
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.listView1);
-            this.groupBox1.Font = new System.Drawing.Font("Segoe Print", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.groupBox1.Controls.Add(this.lv_ListaProductos);
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.groupBox1.Location = new System.Drawing.Point(17, 139);
             this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.groupBox1.Name = "groupBox1";
@@ -97,11 +110,11 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Lista de Productos";
             // 
-            // listView1
+            // lv_ListaProductos
             // 
-            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.columnHeader1,
+            this.lv_ListaProductos.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.columnHeader2,
+            this.columnHeader1,
             this.columnHeader3,
             this.columnHeader4,
             this.columnHeader5,
@@ -109,79 +122,80 @@
             this.columnHeader7,
             this.columnHeader8,
             this.columnHeader9});
-            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.listView1.Location = new System.Drawing.Point(4, 37);
-            this.listView1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(1271, 571);
-            this.listView1.TabIndex = 0;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            this.listView1.View = System.Windows.Forms.View.Details;
+            this.lv_ListaProductos.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lv_ListaProductos.Location = new System.Drawing.Point(4, 29);
+            this.lv_ListaProductos.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.lv_ListaProductos.Name = "lv_ListaProductos";
+            this.lv_ListaProductos.Size = new System.Drawing.Size(1271, 579);
+            this.lv_ListaProductos.TabIndex = 0;
+            this.lv_ListaProductos.UseCompatibleStateImageBehavior = false;
+            this.lv_ListaProductos.View = System.Windows.Forms.View.Details;
             // 
             // columnHeader1
             // 
             this.columnHeader1.Text = "Articulo";
+            this.columnHeader1.Width = 140;
             // 
             // columnHeader2
             // 
             this.columnHeader2.Text = "Nombre";
-            this.columnHeader2.Width = 100;
+            this.columnHeader2.Width = 140;
             // 
             // columnHeader3
             // 
             this.columnHeader3.Text = "Tipo";
-            this.columnHeader3.Width = 100;
+            this.columnHeader3.Width = 140;
             // 
             // columnHeader4
             // 
             this.columnHeader4.Text = "Cantidad";
-            this.columnHeader4.Width = 100;
+            this.columnHeader4.Width = 140;
             // 
             // columnHeader5
             // 
             this.columnHeader5.Text = "Precio";
-            this.columnHeader5.Width = 100;
+            this.columnHeader5.Width = 140;
             // 
             // columnHeader6
             // 
             this.columnHeader6.Text = "Fecha Caducidad";
-            this.columnHeader6.Width = 120;
+            this.columnHeader6.Width = 140;
             // 
             // columnHeader7
             // 
             this.columnHeader7.Text = "Ubicacion";
-            this.columnHeader7.Width = 120;
+            this.columnHeader7.Width = 140;
             // 
             // columnHeader8
             // 
             this.columnHeader8.Text = "Estado";
-            this.columnHeader8.Width = 100;
+            this.columnHeader8.Width = 140;
             // 
             // columnHeader9
             // 
             this.columnHeader9.Text = "Proveedor";
-            this.columnHeader9.Width = 100;
+            this.columnHeader9.Width = 140;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe Print", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label1.Location = new System.Drawing.Point(21, 757);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(214, 31);
+            this.label1.Size = new System.Drawing.Size(186, 25);
             this.label1.TabIndex = 4;
             this.label1.Text = "Cantidad de registros:";
             // 
-            // label2
+            // lbl_CantidadRegistros
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(211, 767);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(47, 25);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "_____";
+            this.lbl_CantidadRegistros.AutoSize = true;
+            this.lbl_CantidadRegistros.Location = new System.Drawing.Point(215, 757);
+            this.lbl_CantidadRegistros.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_CantidadRegistros.Name = "lbl_CantidadRegistros";
+            this.lbl_CantidadRegistros.Size = new System.Drawing.Size(47, 25);
+            this.lbl_CantidadRegistros.TabIndex = 5;
+            this.lbl_CantidadRegistros.Text = "_____";
             // 
             // btn_Regresar
             // 
@@ -197,12 +211,12 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1313, 813);
+            this.ClientSize = new System.Drawing.Size(1313, 798);
             this.Controls.Add(this.btn_Regresar);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbl_CantidadRegistros);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.cmb_FiltrarStock);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
@@ -218,9 +232,9 @@
 
         private Button button1;
         private Button button2;
-        private ComboBox comboBox1;
+        private ComboBox cmb_FiltrarStock;
         private GroupBox groupBox1;
-        private ListView listView1;
+        private ListView lv_ListaProductos;
         private ColumnHeader columnHeader1;
         private ColumnHeader columnHeader2;
         private ColumnHeader columnHeader3;
@@ -231,7 +245,7 @@
         private ColumnHeader columnHeader8;
         private ColumnHeader columnHeader9;
         private Label label1;
-        private Label label2;
+        private Label lbl_CantidadRegistros;
         private Button btn_Regresar;
     }
 }
--- ProyectoPOE/InventarioStock.cs
+++ ProyectoPOE/InventarioStock.cs
@@ -22,5 +22,149 @@ namespace ProyectoPOE
             new MenuPage().Show();
             this.Hide();
         }
+
+        private void cmb_FiltrarStock_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (cmb_FiltrarStock.SelectedIndex)
+            {
+                case 0:
+                {
+                    AbarrotesInventario();
+                    break;
+                }
+                case 1:
+                {
+                    lv_ListaProductos.Items.Clear();
+                    break;
+                }
+                case 2:
+                {
+                    lv_ListaProductos.Items.Clear();
+                    break;
+                }
+            }
+        }
+
+        private void AbarrotesInventario()
+        {
+            string[][] str1 = new string[10][];
+            str1[0] = new string[]
+            {
+                "Sal",
+                "Abarrotes",
+                "$85",
+                "90",
+                "$0.74",
+                "7/10/2025",
+                "Bodega 2",
+                "Buen Estado",
+                "Nestle"
+            };
+            str1[1] = new string[]
+            {
+                "Salsas envasadas",
+                "Abarrotes",
+                "$99",
+                "65",
+                "$1.00",
+                "7/10/2025",
+                "Bodega 2",
+                "Buen Estado",
+                "Nestle"
+            };
+            str1[2] = new string[]
+            {
+                "Sazonadores",
+                "Abarrotes",
+                "$56",
+                "59",
+                "$2.07",
+                "7/10/2025",
+                "Bodega 2",
+                "Buen Estado",
+                "Nestle"
+            };
+            str1[3] = new string[]
+            {
+                "Sopas en sobre",
+                "Abarrotes",
+                "$79",
+                "65",
+                "$1.50",
+                "7/10/2025",
+                "Bodega 2",
+                "Buen Estado",
+                "Nestle"
+            };
+            str1[4] = new string[]
+            {
+                "Cajeta",
+                "Abarrotes",
+                "$67",
+                "88",
+                "$3.00",
+                "7/10/2025",
+                "Bodega 2",
+                "Buen Estado",
+                "Nestle"
+            };
+            str1[5] = new string[]
+            {
+                "Catsup",
+                "Abarrotes",
+                "$82",
+                "92",
+                "$1.50",
+                "7/10/2025",
+                "Bodega 2",
+                "Buen Estado",
+                "Nestle"
+            };
+            str1[6] = new string[]
+            {
+                "Mayonesa",
+                "Abarrotes",
+                "$87",
+                "77",
+                "$2.00",
+                "7/10/2025",
+                "Bodega 2",
+                "Buen Estado",
+                "Nestle"
+            };
+            str1[7] = new string[]
+            {
+                "Mermelada",
+                "Abarrotes",
+                "$83",
+                "54",
+                "$4.00",
+                "7/10/2025",
+                "Bodega 2",
+                "Buen Estado",
+                "Nestle"
+            };
+            str1[8] = new string[]
+            {
+                "Miel",
+                "Abarrotes",
+                "$85",
+                "77",
+                "$6.00",
+                "7/10/2025",
+                "Bodega 2",
+                "Buen Estado",
+                "Nestle"
+            };
+
+            ListViewItem[] items = new ListViewItem[str1.Length];
+            {
+                for (int i = 0; i < str1.Length; i++)
+                {
+                    items[i] = new ListViewItem(str1[i]);
+                }
+                lv_ListaProductos.Items.AddRange(items);
+            }
+        }
     }
 }
